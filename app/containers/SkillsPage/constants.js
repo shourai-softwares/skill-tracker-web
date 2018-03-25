024f1c2e9d328296a3903430880724859676dab0
Duilio Felix <duilio@lipperhub.com>
2018-03-25 15:58:07 -0300

    Adding envfile to dev

--- app/containers/SkillsPage/constants.js
+++ app/containers/SkillsPage/constants.js
@@ -4,12 +4,12 @@
  *
  */
 
-export const ADD_SKILL_URL = 'http://localhost:8000/skills/new';
+export const ADD_SKILL_URL = `${process.env.API_URL}/skills/new`;
 export const ADD_SKILL_REQUESTED = 'app/SkillsPage/ADD_SKILL_REQUESTED';
 export const ADD_SKILL_SUCCEEDED = 'app/SkillsPage/ADD_SKILL_SUCCEEDED';
 export const ADD_SKILL_FAILED = 'app/SkillsPage/ADD_SKILL_FAILED';
 
-export const GET_SKILL_URL = 'http://localhost:8000/skills';
+export const GET_SKILL_URL = `${process.env.API_URL}/skills`;
 export const GET_SKILL_REQUESTED = 'app/SkillsPage/GET_SKILL/REQUESTED';
 export const GET_SKILL_SUCCEEDED = 'app/SkillsPage/GET_SKILL/SUCCEEDED';
 export const GET_SKILL_FAILED = 'app/SkillsPage/GET_SKILL/FAILED';
